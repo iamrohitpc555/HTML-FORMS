36ffcb6e0211e742e14408ef0e079fe515d3cbe2
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-03-26 22:43:04 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,27 +1,24 @@
-// Validating Name Entry
-        const text = document.querySelector('#text');
-        const textError = document.querySelector('.text-error');
-        text.addEventListener('input', function () {
-            let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}([ ][A-Z]{1}[a-z]{2,}){0,}$');
-            if (nameRegex.test(text.value))
-                textError.textContent = "";
-            else textError.textContent = "Name Is Incorrect";
-        });
+//name entry
+        const text = document.querySelector('#text')
+        const textError = document.querySelector('.text-error')
+        text.addEventListener('input',function() {
+            let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
+            if(nameRegex.test(text.value))
+            textError.textContent = "Name is Incorrect"
+        })
+//display slider value
+        const salary = document.querySelector('#salary')
+        const output = document.querySelector('.salary-output')
+        output.textContent = salary.value
+        salary.addEventListener('input',function() {
+            output.textContent = salary.value
+        })
 
-        // Displaying Slider Values
-        const salary = document.querySelector('#salary');
-        const output = document.querySelector('.salary-output');
-        // output.textContent = salary.value;
-        salary.addEventListener('input', function () {
-            output.textContent = salary.value;
-        });
-
-        // Validating Email Entry
-        const email = document.querySelector('#email');
-        const emailError = document.querySelector('.email-error');
-        email.addEventListener('input', function () {
-            let emailRegex = RegExp("^[a-z0-9]+(([\\.+-][a-z0-9]{1,})?)+@[a-z0-9]+\\.([a-z]{2,4})+((\\.[a-z]{2,4})?)$");
-            if (emailRegex.test(email.value))
-                emailError.textContent = "";
-            else emailError.textContent = "Incorrect Email-Id";
-        });
+        //validate email
+        const text = document.querySelector('#text')
+        const textError = document.querySelector('.text-error')
+        text.addEventListener('input',function() {
+            let emailRegex= RegExp("^[a-z0-9]+(([\\.+-][a-z0-9]{1,})?)+@[a-z0-9]+\\.([a-z]{2,4})+((\\.[a-z]{2,4})?)$")
+            if(emailRegex.test(text.value))
+            textError.textContent = "Email is Invalid"
+        })
